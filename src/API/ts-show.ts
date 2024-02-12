44257ba84bb0e1fb9be67caad3a5d7dfbe601e42
Fatouche <foucaud.giovanny@gmail.com>
2024-02-12 22:46:42 +0100

    env file

--- src/API/ts-show.ts
+++ src/API/ts-show.ts
@@ -1,25 +1,25 @@
 import axios from "axios";
 import { PopularResult } from "../types/tv-show";
-import { API_KEY_PARAM, BASE_URL, LANG_PARAM } from "../config";
+import { BASE_URL, LANG_PARAM } from "../config";
 
 export class TVShowAPI {
   static async fetchPopulars() {
     const response = await axios.get<PopularResult>(
-      `${BASE_URL}tv/popular${API_KEY_PARAM}${LANG_PARAM}`
+      `${BASE_URL}tv/popular?api_key=${process.env.REACT_APP_API_KEY_PARAM}${LANG_PARAM}`
     );
     return response.data.results;
   }
 
   static async fetchRecommendations(tvShowId: string) {
     const response = await axios.get<PopularResult>(
-      `${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}${LANG_PARAM}`
+      `${BASE_URL}tv/${tvShowId}/recommendations?api_key=${process.env.REACT_APP_API_KEY_PARAM}${LANG_PARAM}`
     );
     return response.data.results;
   }
 
   static async fetchByTitle(title: string) {
     const response = await axios.get<PopularResult>(
-      `${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}${LANG_PARAM}`
+      `${BASE_URL}search/tv?api_key=${process.env.REACT_APP_API_KEY_PARAM}&query=${title}${LANG_PARAM}`
     );
     return response.data.results;
   }
